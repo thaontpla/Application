b7d32d871836e689b74c06a262b59c82aae83371
thaontpla <thaontp.la@gmail.com>
2023-07-09 15:51:04 +0700

    init

---
+++ src/test/java/com/testfinal/tasks/OpenTheApplication.java
@@ -0,0 +1,18 @@
+package com.testfinal.tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Open;
+import net.thucydides.core.annotations.Step;
+import com.testfinal.ui.TechmasterPage;
+
+public class OpenTheApplication implements Task {
+    TechmasterPage techmasterPage;
+    @Override
+    @Step("Open the application")
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Open.browserOn().the(techmasterPage)
+        );
+    }
+}
---
+++ src/test/java/com/testfinal/ui/TechmasterPage.java
@@ -0,0 +1,7 @@
+package com.testfinal.ui;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("https://techmaster.vn/")
+public class TechmasterPage extends PageObject {}
